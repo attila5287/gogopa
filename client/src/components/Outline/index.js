a5cda9a1cf54547a51c7bce5846334d25fbfc90a
attila5287 <atiturkoz@hotmail.com>
2021-07-23 14:54:16 -0600

    rmv forms from outline

--- client/src/components/Outline/index.js
+++ client/src/components/Outline/index.js
@@ -9,37 +9,26 @@ export default function Outline(){
 					<i className='fas fa-list-ul mx-1'></i>
 					View, Edit or Delete Items
 				</b>
-			</p>
+      </p>
+      <div className="row">
 			{accounts().map((a) => (
-				<>
-					<h4 className='far fa-minus-square mt-3 mb-0'> {a.category}</h4>
-					<ul className='list-group bg-transparent border-0 mx-5 px-5'>
+				<div className="col-6">
+					<h4 className='far fa-minus-square mt-3 mb-2 text-uppercase'> {a.category}</h4>
+					<ul className='list-group bg-transparent border-0 shadow-none'>
 						{a.accounts.map((f) => (
-							<li className='list-group-item d-flex flex-row justify-content-between align-items-center py-0 my-1 border-0'>
-								<h6 className='w-100'>
+							<li className='list-group-item py-0 my-1 border-0'>
+								<h5 className='w-100'>
 									{' '}
-									<i className='far fa-plus-square mx-2'></i>
+									<i className='far fa-plus-square mx-1'></i>
 									{f}
-								</h6>
+								</h5>
 
-								<div className='input-group'>
-									<div className='input-group-prepend'>
-										<span className='input-group-text'>$</span>
-									</div>
-									<input
-										type='text'
-										className='form-control form-control-dark border-0 py-0'
-										aria-label='Amount (to the nearest dollar)'
-									></input>
-									<div className='input-group-append'>
-										<span className='input-group-text'>.00</span>
-									</div>
-								</div>
 							</li>
 						))}
 					</ul>
-				</>
+				</div>
 			))}
+      </div>
 		</>
 	);
 };
