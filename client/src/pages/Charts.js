c8d131d75307d7fbdb61f92ba445c43f202cae5a
attila5287 <atiturkoz@hotmail.com>
2021-07-30 12:14:58 -0600

    charts

--- client/src/pages/Charts.js
+++ client/src/pages/Charts.js
@@ -8,6 +8,61 @@ import helpers from '../utils/helpers';
 import API from '../utils/API';
 import { useStoreContext } from '../utils/GlobalState';
 import { SET_CURRENT_FINANCIAL } from '../utils/actions';
+import { Doughnut, Bar } from '@iftek/react-chartjs-3';
+
+const rand = () => Math.floor(Math.random() * 3) + 1;
+
+const myData = [
+	{
+		type: 'line',
+		label: 'Profit/Loss',
+		data: [1311751, 40140, 842227, 797652],
+		backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+		borderColor: ['rgba(255, 99, 132, 1)'],
+		borderWidth: 1
+	},
+	{
+		label: 'Gross Sales',
+		data: [3954888, 3111456, 3157123, 3541321],
+		backgroundColor: ['rgba(54, 162, 235, 0.2)'],
+		borderColor: ['rgba(54, 162, 235, 1)'],
+		borderWidth: 1
+	},
+	{
+		label: 'Fixed Cost',
+		data: [1519681, 1911658, 1155668, 1584441],
+		backgroundColor: ['rgba(255, 206, 86, 0.2)'],
+		borderColor: ['rgba(255, 206, 86, 1)'],
+		borderWidth: 1
+	},
+	{
+		label: 'Operations Cost',
+		data: [1123456, 1159658, 1159228, 1159228],
+		backgroundColor: ['rgba(75, 192, 192, 0.2)'],
+		borderColor: ['rgba(75, 192, 192, 1)'],
+		borderWidth: 1
+	}
+];
+
+const genData = () => ({
+	labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+	// number (pixels) or 'flex'
+	barThickness: '11',
+
+	datasets: [myData[0], myData[rand()]]
+});
+
+const options = {
+	scales: {
+		yAxes: [
+			{
+				ticks: {
+					beginAtZero: true
+				}
+			}
+		]
+	}
+};
 
 const Charts = (props) => {
 	const [state, dispatch] = useStoreContext();
